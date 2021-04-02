ed613048ef100aa5e4a4247a3c39d361d6e2d6ab
Othman Shareef <othmanosx@gmail.com>
2021-04-02 16:18:27 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,9 +3,20 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
-    </div>
+    <>
+        <h1>Movies Page</h1>
+      {movies.map((ele) => (
+          <div >
+              <p>Name: {ele.title}</p>
+              <p>Time: {ele.time}</p>
+              <ul>
+                  {ele.genres.map((e) => (
+                      <li >{e}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
+    </>
   );
 };
 
